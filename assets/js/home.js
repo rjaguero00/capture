ad4a90af8e2299bc967a470fabc8ea41d6d24d46
Roberto Aguero <rjaguero00@gmail.com>
2019-05-20 22:55:15 -0500

    header/nav

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,31 @@
 // js for home page
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
+// Init Scrollspy
+$('body').scrollspy({ target: '#main-nav' });
+// Smooth Scrolling
+$("#main-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
+});
