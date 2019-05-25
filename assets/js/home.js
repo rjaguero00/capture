d81c25e3dad7ee1fa8c31ed8e0ec809d82c0cdda
Roberto Aguero <rjaguero00@gmail.com>
2019-05-24 22:29:15 -0500

    insta feed/smooth scroll

--- assets/js/home.js
+++ assets/js/home.js
@@ -29,3 +29,17 @@ $("#main-nav a").on('click', function (event) {
         });
     }
 });
+
+
+// Smooth Scrolling
+$("#main-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
+});
